2393825b56b059e6c8c6a00317ccc24b2eb31f53
yoshisada <suematsuryan@gmail.com>
2025-03-08 22:43:50 -0800

    adding frontend pipeline deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
 	images: {
 		domains: ["www.gravatar.com", "cryptologos.cc", "static.vecteezy.com", "dd.dexscreener.com", "kifi-acceleration-25-icons.s3.us-east-1.amazonaws.com"],
 	},
+	output: 'standalone',
 };
 
 module.exports = nextConfig;
